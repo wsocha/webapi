26b56824775dc0f6e51a3320c1e7e5f41490c3d4
Wojciech Socha <socha.wojtek@gmail.com>
2022-12-19 16:28:10 +0100

    Configure console logging and xml support

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,10 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddXmlSerializerFormatters();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConsole();
+});
 
 var app = builder.Build();
 
